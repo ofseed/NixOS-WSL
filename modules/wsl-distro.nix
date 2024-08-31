3858b532c18e2539ab8a70a095a4af415c778c22
Yi Ming <ofseed@foxmail.com>
2024-08-31 06:57:52 +0000

    Modify default username

--- modules/wsl-distro.nix
+++ modules/wsl-distro.nix
@@ -17,7 +17,7 @@ in
     };
     defaultUser = mkOption {
       type = str;
-      default = "nixos";
+      default = "ofseed";
       description = "The name of the default user";
     };
     populateBin = mkOption {
